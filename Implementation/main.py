ebf32f5d87308fc370870a1e7eff03d2c8a66acd
Josephson Junior <josephson-junior.razafindratsiza@eleve.ensai.fr>
2025-11-26 16:34:56 +0100

    add implementation of models.py implementation

---
+++ Implementation/main.py
@@ -0,0 +1,45 @@
+import argparse
+import torch
+import random
+import numpy as np
+from server import Server
+
+def set_seed(seed=42):
+    """
+    Sets the random seed for reproducibility.
+    This ensures that every time you run the code, the 'random' initialization
+    of weights and data splits is exactly the same. 
+    Crucial for debugging!
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    # Ensure determinstic behavior in CuDNN (for Nvidia GPUs)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Federated Learning Simulation")
+    
+    parser.add_argument('--rounds', type=int, default=3, 
+                        help="Number of Global Communication Rounds")
+    
+    parser.add_argument('--clients', type=int, default=5, 
+                        help="Number of Clients in the federation")
+    
+    parser.add_argument('--seed', type=int, default=42, 
+                        help="Random seed for reproducibility")
+
+    args = parser.parse_args()
+
+    print(f"--- Configuration: {args.clients} Clients, {args.rounds} Rounds ---")
+    set_seed(args.seed)
+
+    server = Server(num_clients=args.clients, rounds=args.rounds)
+
+    server.train()
+
+if __name__ == "__main__":
+    main()
